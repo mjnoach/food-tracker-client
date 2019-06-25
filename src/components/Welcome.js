7285c2e50d824139785a3e7b3675474b44ffc57c
Andrzej Sienkiewicz <a.siennkiewicz@gmail.com>
2019-06-25 21:37:57 +0200

    Init

---
+++ src/components/Welcome.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Welcome extends Component {
+  render() {
+    return(
+      <div>
+        <h1>food tracker</h1>
+      </div>
+    )
+  }
+}
